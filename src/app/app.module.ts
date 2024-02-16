b3db0f114bcd7b72f256e3421d0f8eb3ba50f899
shagun-abjima <153979823+shagun-abjima@users.noreply.github.com>
2024-02-16 18:52:59 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { CustomInterceptor} from './service/custom.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+     provide:HTTP_INTERCEPTORS,
+     useClass:CustomInterceptor,
+     multi:true
+    }
+ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
